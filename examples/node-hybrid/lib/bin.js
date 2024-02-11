88a85777293e9333ec06da5fad1fb735623891eb
Shigma <shigma10826@gmail.com>
2024-02-12 01:33:03 +0800

    test: add tsc output

--- examples/node-hybrid/lib/bin.js
+++ examples/node-hybrid/lib/bin.js
@@ -1,18 +1,4 @@
-var __defProp = Object.defineProperty;
-var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
-
-// src/foo.ts
-function foo() {
-  console.log("foo");
-}
-__name(foo, "foo");
-
-// src/bar.ts
-function bar() {
-  console.log("bar");
-}
-__name(bar, "bar");
-
 // src/bin.ts
+import { bar, foo } from "./index.js";
 foo();
 bar();
